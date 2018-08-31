68127ad42ee7dba4ffc472516ec8f62b2347d8bc
The Developer <leshiktk@me.com>
2018-09-01 00:03:19 +0300

    Update #3
    Added a smart part for the cest prefiller

---
+++ main/mac/chestgen_gui.py
@@ -0,0 +1,35 @@
+from Tkinter import *
+########################################
+root = Tk()
+root.title("Chest pref, borderwidth=10, bg='gray'iller")
+##########################################################################################
+slot1 = Frame(root, width=50, height=50, borderwidth=10, bg='gray').grid(row=0, column=1)#
+slot2 = Frame(root, width=50, height=50, borderwidth=10, bg='gray').grid(row=0, column=2)#
+slot3 = Frame(root, width=50, height=50, borderwidth=10, bg='gray').grid(row=0, column=3)#
+slot4 = Frame(root, width=50, height=50, borderwidth=10, bg='gray').grid(row=0, column=4)#
+slot5 = Frame(root, width=50, height=50, borderwidth=10, bg='gray').grid(row=0, column=5)#
+slot6 = Frame(root, width=50, height=50, borderwidth=10, bg='gray').grid(row=0, column=6)#
+slot7 = Frame(root, width=50, height=50, borderwidth=10, bg='gray').grid(row=0, column=7)#
+slot8 = Frame(root, width=50, height=50, borderwidth=10, bg='gray').grid(row=0, column=8)#
+##########################################################################################
+slot11 = Frame(root, width=50, height=50, borderwidth=10, bg='gray').grid(row=1, column=1)#
+slot12 = Frame(root, width=50, height=50, borderwidth=10, bg='gray').grid(row=1, column=2)#
+slot13 = Frame(root, width=50, height=50, borderwidth=10, bg='gray').grid(row=1, column=3)#
+slot14 = Frame(root, width=50, height=50, borderwidth=10, bg='gray').grid(row=1, column=4)#
+slot15 = Frame(root, width=50, height=50, borderwidth=10, bg='gray').grid(row=1, column=5)#
+slot16 = Frame(root, width=50, height=50, borderwidth=10, bg='gray').grid(row=1, column=6)#
+slot17 = Frame(root, width=50, height=50, borderwidth=10, bg='gray').grid(row=1, column=7)#
+slot18 = Frame(root, width=50, height=50, borderwidth=10, bg='gray').grid(row=1, column=8)#
+###########################################################################################
+slot21 = Frame(root, width=50, height=50, borderwidth=10, bg='gray').grid(row=2, column=1)#
+slot22 = Frame(root, width=50, height=50, borderwidth=10, bg='gray').grid(row=2, column=2)#
+slot23 = Frame(root, width=50, height=50, borderwidth=10, bg='gray').grid(row=2, column=3)#
+slot24 = Frame(root, width=50, height=50, borderwidth=10, bg='gray').grid(row=2, column=4)#
+slot50 = Frame(root, width=50, height=50, borderwidth=10, bg='gray').grid(row=2, column=5)#
+slot26 = Frame(root, width=50, height=50, borderwidth=10, bg='gray').grid(row=2, column=6)#
+slot27 = Frame(root, width=50, height=50, borderwidth=10, bg='gray').grid(row=2, column=7)#
+slot28 = Frame(root, width=50, height=50, borderwidth=10, bg='gray').grid(row=2, column=8)#
+###########################################################################################
+
+########################################
+root.mainloop()
--- main/mac/main_gui.py
+++ main/mac/main_gui.py
@@ -1,4 +1,5 @@
 import text_gui as text
+import chestgen_gui as chestgen
 from AnimatedGif import *
 from Tkinter import *
 from os import getcwd
@@ -23,8 +24,13 @@ def to_text():
     text.start()
 
 
+def to_chestgen():
+    chestgen.start()
+
+
 ########################################
 Button(root, text="Text compiler", command=to_text).grid(row=1, column=0, sticky=W)
 Button(root, text="NBT/Data tags", command=none).grid(row=1, column=1, sticky=W)
+Button(root, text="Prefilled chest generator", command=to_chestgen).grid(row=1, column=1, sticky=W)
 ########################################
 root.mainloop()
